1a33d31314bf94cf5ae3ce2d3ab99a98d3b91e9d
rajadavid423 <59241996+rajadavid423@users.noreply.github.com>
2022-11-11 22:32:12 +0530

    Init
    Employee, Customer, Category, and Products CRUD completed

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\API\UserAuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('login', [UserAuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    //Auth
+    Route::get('logout', [UserAuthController::class, 'logout']);
+    Route::post('change-password', [UserAuthController::class, 'changePassword']);
+    Route::get('profile', [UserAuthController::class, 'profile']);
+
+});
